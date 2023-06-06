16b00a69b84f7d87e2b7308310c2f767218dbf55
severo <oliveira.severo@gmail.com>
2023-06-06 09:27:52 -0300

    style: :fire: remove css nao usado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
 
 // Componentes relacionados à seção "Explore"
 import Explore from './pages/explore_pages/Explore';
